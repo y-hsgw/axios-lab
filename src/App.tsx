e72f78299af39a2c2da17ef4a372df9a9a6d03dc
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-09-22 22:37:40 +0900

    fetchTodoの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import { axios } from "./lib/axios";
 
 function App() {
+  const fetchTodo = useCallback(() => {
+    axios.get("/todos/1", {
+      params: { test: "111 222" },
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchTodo();
+  }, [fetchTodo]);
+
   return <div>Hello World</div>;
 }
 
