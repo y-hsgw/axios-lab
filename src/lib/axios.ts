2451c18f3ff0a1d7a5aaba47534c6330afde965e
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-09-23 13:15:26 +0900

    axiosの設定にparamsSerializerの追加

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import Axios from "axios";
+import qs from "qs";
 
 export const axios = Axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
+  paramsSerializer: (params) => qs.stringify(params),
 });
