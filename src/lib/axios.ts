afeb9f1ae2c3442d8ea4abcdbfc20d0554470c4d
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-09-26 21:14:50 +0900

    lintのwarning修正

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
-import qs from "qs";
+import { stringify } from "qs";
 
 export const axios = Axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
-  paramsSerializer: (params) => qs.stringify(params),
+  paramsSerializer: (params) => stringify(params),
 });
